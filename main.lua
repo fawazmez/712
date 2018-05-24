ee80961c4f431b7e7f65002d76b955bfd246bbae
fawazmez <fawaz.mezher@ocsbstudent.ca>
2018-05-24 13:31:44 -0400

    Initial commit

---
+++ main.lua
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-- Created By: Fawaz Mezher
+-- Created On: May 2018
+-----------------------------------------------------------------------------------------
+
+local composer = require( 'composer' )
+
+display.setStatusBar(display.HiddenStatusBar)
+
+composer.gotoScene( 'scene1' )
